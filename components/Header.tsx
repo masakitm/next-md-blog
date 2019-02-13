b88ce05d047a93d5561ffbfb9a8364e21c8b1ca3
友塚　正樹 <uu144692@CA4167.local>
2019-02-13 11:49:09 +0900

    add: header styling

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,12 +1,34 @@
 import * as React from "react";
 import Link from "next/link";
 
-const Header = () => (
-  <h1>
-    <Link href="/">
-      <a>Next Markdown Blog</a>
-    </Link>
-  </h1>
+interface Props {
+  name: string;
+  word: string;
+}
+
+const Header = props => (
+  <>
+    <h1
+      style={{
+        fontSize: "2rem",
+        textAlign: "center",
+        margin: "4rem 0 0"
+      }}
+    >
+      <Link href="/">
+        <a>
+          <span
+            style={{
+              color: "#ff9472"
+            }}
+          >
+            {props.name}
+          </span>
+          {props.word}
+        </a>
+      </Link>
+    </h1>
+  </>
 );
 
 export default Header;
