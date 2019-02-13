10d734b1637354d19cbd87127563704e2a812285
友塚　正樹 <uu144692@CA4167.local>
2019-02-13 17:07:27 +0900

    feature: add style & types to header

--- components/Head.tsx
+++ components/Head.tsx
@@ -1,44 +1,41 @@
-import * as React from 'react'
-import NextHead from 'next/head'
-import { string } from 'prop-types'
+import * as React from "react";
+import NextHead from "next/head";
+import { Global, css } from "@emotion/core";
 
-const defaultDescription = ''
-const defaultOGURL = ''
-const defaultOGImage = ''
-
-const Head = props => (
-  <NextHead>
-    <meta charSet="UTF-8" />
-    <title>{props.title || ''}</title>
-    <meta
-      name="description"
-      content={props.description || defaultDescription}
-    />
-    <meta name="viewport" content="width=device-width, initial-scale=1" />
-    <link rel="icon" sizes="192x192" href="/static/touch-icon.png" />
-    <link rel="apple-touch-icon" href="/static/touch-icon.png" />
-    <link rel="mask-icon" href="/static/favicon-mask.svg" color="#49B882" />
-    <link rel="icon" href="/static/favicon.ico" />
-    <meta property="og:url" content={props.url || defaultOGURL} />
-    <meta property="og:title" content={props.title || ''} />
-    <meta
-      property="og:description"
-      content={props.description || defaultDescription}
-    />
-    <meta name="twitter:site" content={props.url || defaultOGURL} />
-    <meta name="twitter:card" content="summary_large_image" />
-    <meta name="twitter:image" content={props.ogImage || defaultOGImage} />
-    <meta property="og:image" content={props.ogImage || defaultOGImage} />
-    <meta property="og:image:width" content="1200" />
-    <meta property="og:image:height" content="630" />
-  </NextHead>
-)
-
-Head.propTypes = {
-  title: string,
-  description: string,
-  url: string,
-  ogImage: string
+interface Props {
+  title?: string;
+  description?: string;
 }
 
-export default Head
+const Head = (props: Props) => (
+  <NextHead>
+    <meta charSet="UTF-8" />
+    <title>{props.title || ""}</title>
+    <meta name="description" content={props.description} />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <meta property="og:description" content={props.description} />
+    <meta name="twitter:card" content="summary_large_image" />
+    <meta property="og:image:width" content="1200" />
+    <meta property="og:image:height" content="630" />
+    <link
+      rel="stylesheet"
+      href="https://cdnjs.cloudflare.com/ajax/libs/meyer-reset/2.0/reset.min.css"
+    />
+    <link
+      href="https://fonts.googleapis.com/css?family=Roboto"
+      rel="stylesheet"
+    />
+    <Global
+      styles={css`
+        * {
+          margin: 0;
+          padding: 0;
+          text-decoration: none;
+          box-sizing: border-box;
+        }
+      `}
+    />
+  </NextHead>
+);
+
+export default Head;
