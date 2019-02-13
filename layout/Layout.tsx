7df5475a7c5dadb4cae8b6b2222008ffc5369d1d
友塚　正樹 <uu144692@CA4167.local>
2019-02-13 17:06:36 +0900

    feature: add props to layout component

--- layout/Layout.tsx
+++ layout/Layout.tsx
@@ -1,13 +1,42 @@
 import * as React from "react";
+import { css } from "@emotion/core";
 import Head from "../components/Head";
 import Header from "../components/Header";
 
-const Layout = ({ children }) => (
-  <div>
-    <Head />
-    <Header />
-    {children}
-  </div>
-);
+interface InitState {
+  pageInfo?: {
+    title: string;
+    name: string;
+    word: string;
+    description: string;
+  };
+}
+
+const initState: InitState = {
+  pageInfo: {
+    title: "Masaki Tomotsuka Blog",
+    name: "Masaki Tomotsuka",
+    word: " Blog",
+    description: "願わくば、その生が愛し愛されんことを"
+  }
+};
+
+const background = css({
+  width: "100vw",
+  height: "100vh"
+});
+
+const Layout = ({ children }) => {
+  return (
+    <div className={background.styles}>
+      <Head
+        title={initState.pageInfo.title}
+        description={initState.pageInfo.description}
+      />
+      <Header name={initState.pageInfo.name} word={initState.pageInfo.word} />
+      {children}
+    </div>
+  );
+};
 
 export default Layout;
